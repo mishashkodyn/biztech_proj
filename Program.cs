c96426099ddfddefec5c2e2a80779698f02c8d02
Mykhaylo Shkodyn <ms@a2software.dk>
2026-02-17 21:05:30 +0200

    //

--- Program.cs
+++ Program.cs
@@ -89,20 +89,20 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<ApplicationDbContext>();
+    //try
+    //{
+    //    var context = services.GetRequiredService<ApplicationDbContext>();
 
-        if (context.Database.GetPendingMigrations().Any())
-        {
-            context.Database.Migrate();
-        }
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while migrating the database.");
-    }
+    //    if (context.Database.GetPendingMigrations().Any())
+    //    {
+    //        context.Database.Migrate();
+    //    }
+    //}
+    //catch (Exception ex)
+    //{
+    //    var logger = services.GetRequiredService<ILogger<Program>>();
+    //    logger.LogError(ex, "An error occurred while migrating the database.");
+    //}
 }
 
     app.UseCors(x => 
